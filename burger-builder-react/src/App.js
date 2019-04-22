84eaf1b322d5aa273fe54f5d43992a57dc535bef
Guru <gurubrahmac@gmail.com>
2019-04-22 17:29:50 +0530

    Create the Functional Component added to App Component

--- burger-builder-react/src/App.js
+++ burger-builder-react/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
-      // <div className="App">
-      // <h1>Hi, I'm a React App</h1>
-      // </div>
-      React.createElement('div',{className:"App"},
-      React.createElement('h1',null,'Hi, I\'m a React App'))
+      <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p>This is Realy Working!!!</p>
+      <Person />
+      </div>
+    
     );
   }
 }
