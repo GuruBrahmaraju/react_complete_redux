ecaa317419ba86a6619e3d37fd7f0002897de9fe
Guru <gurubrahmac@gmail.com>
2019-04-24 12:34:38 +0530

    Bainding the Data two way Process

--- burger-builder-react/src/App.js
+++ burger-builder-react/src/App.js
@@ -26,18 +26,30 @@ class App extends Component {
     })
   }
 
+  nameChangeHandler = event =>{
+    this.setState({
+      person:[
+        {name:'Max', age:"29"},
+        {name:event.target.value, age:"20"},
+        {name:"RaviKumar", age:"19"}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is Realy Working!!!</p>
+      <button onClick={this.switchHandler.bind(this,"ButtonClick")}>Switch</button>
      <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
      <Person name={this.state.person[1].name}
               age={this.state.person[1].age}
               click={this.switchHandler.bind(this,"PersonCLICK")}
+              changed={this.nameChangeHandler}
       >Business Developement Team</Person>
      <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
-     <button onClick={this.switchHandler.bind(this,"ButtonClick")}>Switch</button>
+     
    
       </div>
     
